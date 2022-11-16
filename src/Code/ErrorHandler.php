3f2b8aeca03f56abab07649a87ce01c5ae035437
lovefc <fcphp@qq.com>
2022-11-16 19:55:01 +0800

    优化格式

--- src/Code/ErrorHandler.php
+++ src/Code/ErrorHandler.php
@@ -2,12 +2,12 @@
 /*
  * @Author       : lovefc
  * @Date         : 2022-11-10 10:25:54
- * @LastEditTime : 2022-11-10 12:35:01
+ * @LastEditTime : 2022-11-16 19:54:42
  */
 
 namespace FC\Code;
 
-define('E_FATAL', E_ERROR | E_USER_ERROR |  E_CORE_ERROR | E_COMPILE_ERROR | E_RECOVERABLE_ERROR| E_PARSE);
+define('E_FATAL', E_ERROR | E_USER_ERROR |  E_CORE_ERROR | E_COMPILE_ERROR | E_RECOVERABLE_ERROR | E_PARSE);
 
 trait ErrorHandler
 {
@@ -15,7 +15,7 @@ trait ErrorHandler
     public function fatalHandler()
     {
         $error = error_get_last();
-        if ($error && ($error["type"]===($error["type"] & E_FATAL))) {
+        if ($error && ($error["type"] === ($error["type"] & E_FATAL))) {
             $errno   = $error["type"];
             $errfile = $error["file"];
             $errline = $error["line"];
@@ -27,7 +27,7 @@ trait ErrorHandler
     // 获取所有的error
     public function errorHandler($errno, $errstr, $errfile, $errline)
     {
-        $log = date("Y/m/d H:i:s")." :".$errstr.$errfile.$errline.PHP_EOL;
+        $log = date("Y/m/d H:i:s") . " :" . $errstr . "-" . $errfile . "-" . $errline . PHP_EOL;
         if (method_exists($this, 'errorLog')) {
             $this->errorLog($log);
         }
@@ -36,7 +36,7 @@ trait ErrorHandler
     // 获取所有的异常,会中断执行,这里没有使用
     public function errorException($exception)
     {
-        $log = date("Y/m/d H:i:s")." :".$exception->getMessage().PHP_EOL;
+        $log = date("Y/m/d H:i:s") . " :" . $exception->getMessage() . PHP_EOL;
         if (method_exists($this, 'errorLog')) {
             $this->errorLog($log);
         }
